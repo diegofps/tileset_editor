7fa69df916d3f5874cedfe0d719ca407d76b8c6e
Diego Souza <diegofpsouza@gmail.com>
2023-10-11 21:56:49 -0300

    Adding a few more basic fragment structures

--- TilesetEditor/fragmenttilepreview.cpp
+++ TilesetEditor/fragmenttilepreview.cpp
@@ -6,6 +6,8 @@ FragmentTilePreview::FragmentTilePreview(QWidget *parent) :
     ui(new Ui::FragmentTilePreview)
 {
     ui->setupUi(this);
+
+    ui->btOriginal->setChecked(true);
 }
 
 FragmentTilePreview::~FragmentTilePreview()
